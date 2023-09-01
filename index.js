a1baad1b6dc3f089b241733d31d8279eafe4462e
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-09-01 11:08:31 +0600

    second

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/*let Person={
+/*
+//stand along object
+let Person={
     first_name: "Khadija ",
     last_name:"Akter",
     age : 23,
@@ -13,4 +15,17 @@ console.log(Person.age);
 console.log(Person.getfullname());
 */
 
-
+//class er object
+class Person {
+    first_name = "Khadija";
+    last_name = "Akter";
+    age =23;
+    getfullName = ()=>{
+        return this.first_name +" "+ this.last_name;
+    }
+}
+let p1= new Person();
+console.log(p1.first_name);
+console.log(p1.last_name);
+console.log(p1.age);
+console.log(p1.getfullName());
