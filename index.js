f2ea943b1d3cf8eb09ca45ce0b5a2d63ae3620ff
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-09-01 09:45:15 +0600

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+/*let Person={
+    first_name: "Khadija ",
+    last_name:"Akter",
+    age : 23,
+    getfullname:()=>{
+        return Person.first_name +" "+ Person.last_name;
+
+    }
+}
+console.log(Person.first_name);
+console.log(Person.last_name);
+console.log(Person.age);
+console.log(Person.getfullname());
+*/
+
+
